a9c4806c3b84f8d312e032d29114c4704b58709f
Jordi Ricard Onrubia Palacios <jordi.onrubia@bluetab.net>
2023-12-22 14:18:56 +0100

    removed useless comments

--- app/back/db.py
+++ app/back/db.py
@@ -1,7 +1,4 @@
-import pandas as pd
 from sqlalchemy import create_engine
 
-
-# engine = create_engine('sqlite:///../data/database.db')C:\Users\jordi.onrubia_blueta\PycharmProjects\SEPACharges\app\data\database.db
 
 engine = create_engine('sqlite:///app/data/database.db')
